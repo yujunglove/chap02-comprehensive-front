9f34b2c1d90502fc2f1d41f2c330781ff158019d
yujunglove <dbwjdeor1234562@gmail.com>
2023-04-30 23:56:04 +0900

    commit

--- 05_router/003_final/src/pages/Main.js
+++ 05_router/003_final/src/pages/Main.js
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Main() {
+  const [activeMenu, setActiveMenu] = useState("");
+
+  const [selectedMenu, setSelectedMenu] = useState('myPage');
+
+  const handleClick = (menuName) => {
+    setActiveMenu(menuName);
+  };
+  
+
   return (
     <body>
     <div>
@@ -9,40 +19,84 @@ function Main() {
         <div className="circle2"></div>
         <div className="circle3"></div>
         <div className="nemo"></div>
-        <div className="memberName">어트랙션-A 석이현</div>
+        <div className="memberName1">어트랙션-A 석이현</div>
+        <div className="memberName2">000-0000-0000</div>
       </div>
-      <div className="menu" style={{ marginTop: "-20px" }}>
-        <Link to="/movie">
-          <div className="myPage">마이페이지</div>
+      <div className="menu" style={{ marginTop: "-30px"}}>
+      <Link to="/movie">
+      <div
+  className="myPage"
+  style={{
+    backgroundColor: selectedMenu === 'myPage' ? '#4D70ED' : 'transparent',
+    color: selectedMenu === 'myPage' ? '#FFFFFF' : 'initial',
+  }}
+  onClick={() => setSelectedMenu('myPage')}
+>
+  마이페이지
+</div>
+
         </Link>
         <Link to="/mail">
-          <div className="mail">메일</div>
+          <div className="mail">급여 내역</div>
         </Link>
         <Link to="/">
           <div className="home">일정</div>
         </Link>
         <Link to="/">
-          <div className="home">게시판</div>
+          <div className="home"> <img src=""/>게시판</div>
         </Link>
         <Link to="/">
-          <div className="home">직원 검색</div>
+          <div className="home">직원</div>
         </Link>
         <Link to="/">
-          <div className="home">급여 내역</div>
+          <div className="home">장비 & 리프트 관리</div>
         </Link>
         <Link to="/">
-          <div className="home">전자 결재</div>
+          <div className="home" >전자 결재</div>
         </Link>
         <Link to="/">
-          <div className="home">교육 관리</div>
+          <div className="home">직원 교육</div>
         </Link>
       </div>
     </div>
 
-
-
-    <div className="container">
-
+    <div className="menu2">
+    <div class="line"></div>
+      
+  <img className="circle4" src="image/circle.png"></img>
+    </div>
+    <div class="container">
+      <div class="title1">
+      <div class="title2">
+          <b>마이페이지</b>
+        </div>
+        <div
+          class={`navbar2 ${activeMenu === "info" ? "active" : ""}`}
+          onClick={() => handleClick("info")}
+        >
+          나의 정보
+        </div>
+        <div
+          class={`navbar2 ${activeMenu === "todo" ? "active" : ""}`}
+          onClick={() => handleClick("todo")}
+        >
+          할 일
+        </div>
+        <div
+          class={`navbar2 ${activeMenu === "ticket" ? "active" : ""}`}
+          onClick={() => handleClick("ticket")}
+        >
+          식권 조회
+        </div>
+        <div
+          class={`navbar2 ${activeMenu === "board" ? "active" : ""}`}
+          onClick={() => handleClick("board")}
+        >
+          부서 게시판
+        </div>
+          <div class="navbar2"></div>
+          <hr></hr>
+    </div>
     </div>
     </body>
   );
