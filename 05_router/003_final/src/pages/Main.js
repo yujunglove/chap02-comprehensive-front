ce94056d1c6c7367840d26b02fec6ebec7ba5c0c
yujunglove <dbwjdeor1234562@gmail.com>
2023-05-01 17:15:04 +0900

    commit

--- 05_router/003_final/src/pages/Main.js
+++ 05_router/003_final/src/pages/Main.js
@@ -16,6 +16,7 @@ function Main() {
     <div>
       <div className="mainProfil">
         <img className="logo1" src="image/profileLogo.png"/>
+        <div className="loginLogo">로그인</div>
         <div className="circle1"></div>
         <div className="circle2"></div>
         <div className="circle3"></div>
